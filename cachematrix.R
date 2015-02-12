dce235a3aabdeea4c9832d7ff0b78b5c6398a737
OswaldoAreal <ofareal@gmail.com>
2015-02-11 22:11:07 -0200

    new version.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 ## Write a short comment describing this function
-## create a special matrix 
+## create a special matrix  
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   get <- function() x
